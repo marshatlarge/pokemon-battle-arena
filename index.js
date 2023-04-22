fd0925fdc173a0fc7c9c8c6fafa111dcfbb8f646
manielsen13 <marshall.niels@gmail.com>
2023-04-22 10:26:03 -0400

    Add charizard

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import BattleController from "./modules/battle_controller.js";
 //Should have a premade list of movesets for each type of pokmeon, so not to overdo the calls to the API.
 //Also, this way, there will be more control over movesets that make sense for the web app. Avoid moves with status effects or moves that self inflict damage for sake of simplicity
 
+//NOTE- make a class that updates the UI, and make a class that is responsible for performing animations
 
 let move1 = new Move('Fire','Flamethrower',100,100)
 let move2 = new Move('Normal','Tackle',100,100)
