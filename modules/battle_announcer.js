d64fe4046a4fcc77c4d4d095bee2c532aaa3e47e
manielsen13 <marshall.niels@gmail.com>
2023-05-06 19:10:36 -0400

    Add functionality to announcer class for announcement progression

--- modules/battle_announcer.js
+++ modules/battle_announcer.js
@@ -1,156 +1,185 @@
-import UIController from "./ui_controller.js"
-
 class BattleAnnouncer { 
+    
     constructor(){
+        this.announcerBox = document.getElementById("announcerBox")
+        this.selectionBox = document.getElementById("selectionBox")
+        this.actionOptionsList = document.getElementById("actionOptionsList")
+        this.moveOptionsList = document.getElementById("moveOptionsList")
+
+        this.announcementsQueue = []
+
+
     }
 
     announceSupereffective() {
         let text = `It's super effective!`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
-        
-
+        this.announcementsQueue.push(text)
+         
     }
 
     announceMiss() {
         let text = `The attack missed!`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        
     }
 
     announceImmunity(target) {
         let text = `${target.name} is immune. The attack has no effect...`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        
     }
 
-    announceMove(pokemon, move) {
+    announceMove(pokemon, move) { //maybe display immediately without needing to cycle through
         let text = `${pokemon.name} used ${move.name}!`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
+        
     }
 
     announceCrit() {
         let text = `It's a critical hit!`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+       
     }
 
     announceResistance() {
         let text = `It's not very effective.`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        
     }
 
-    announceStartOfBattle(){
+    announceStartOfBattle(){ //perhaps display this immediately
         let text = `A wild trainer appeared with a random Pokémon from the PokéAPI!`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
+        
     }
 
     announceEnemyThrow(pokemon){
         let text = `The trainer throws out ${pokemon.name}!`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        console.log(this.announcementsQueue.length)
+      
     }
 
     announceUserThrow(pokemon) {
         let text = `Go get 'em ${pokemon.name}!`
         console.log(text) 
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        console.log(this.announcementsQueue.length)
+       
     }
 
     announcePotionUse(pokemon) {
         let text = `${pokemon.name} was healed!`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
+        
     }
 
     announceZeroPotions() {
         let text = `Oops! Unfortunately, you're all out of potions.`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
+      
     }
 
     announceHealthFull(pokemon) {
         let text = `${pokemon.name} is already at full health.`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
+    
     }
 
     announceThrowPokeballError() {
         let text = `Sorry, you can't catch an opposing trainer's Pokemon.`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
+  
     }
 
     announceRunAway() {
         let text = `You ran away!`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
+       
     }
 
     announceBattleOver() {
         let text = `The battle is over.`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+     
     }
 
     announceInvalidMove() {
         let text = `That isn't a valid move.`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayFullAnnouncerBox()
-        this.displayForSetTime()
+        this.announcementsQueue.push(text)
+        this.displayActionOptionsBox()
     }
 
-    announceActionPrompt() {
+    announceActionPrompt() { //can probably just put this on the side regardless
         let text = `What would you like to do?`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayActionOptionsBox()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
+        this.displayActionOptionsBox() 
     }
 
-    announceMovePrompt(pokemon) {
+    announceMovePrompt(pokemon) { //can also put this on the side regardless
         let text = `What would you like ${pokemon.name} to use?`
         console.log(text)
-        UIController.updateAnnouncerBoxText(text)
-        UIController.displayMoveOptionsBox()
+        this.announcementsQueue.push(text)
+        this.updateAnnouncerBoxText()
     }
 
-    displayForSetTime(){
-        wait(2000);
+
+    updateAnnouncerBoxText() {
+        
+        if(this.announcementsQueue.length > 0) {
+            this.displayFullAnnouncerBox()
+            this.announcerBox.textContent = this.announcementsQueue.shift()
+        }
+        else {
+            this.announceActionPrompt() 
+        } 
     }
+
+    displayFullAnnouncerBox() {
+        this.selectionBox.style.display = "none"
+    }
+
+    displayMoveOptionsBox() {
+        this.selectionBox.style.display = "flex"
+        this.actionOptionsList.style.display = "none"
+        this.moveOptionsList.style.display = "grid"
+        return
+
+    }
+
+    displayActionOptionsBox() {
+        this.selectionBox.style.display = "flex"
+        this.moveOptionsList.style.display = "none"
+        this.actionOptionsList.style.display = "grid"
+        return
+
+    }
+
+
 
 }
 
