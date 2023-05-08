eb06c70cdc3c9754ef85e3191d2d634084032da5
manielsen13 <marshall.niels@gmail.com>
2023-05-08 12:25:27 -0400

    Add menu flash animatins to their own separate function

--- modules/menu_ui_controller.js
+++ modules/menu_ui_controller.js
@@ -13,25 +13,30 @@ export default class MenuUIController {
 
     configureButtons(){
 
+        //Emphasizes button if user is tapping or clicking elsewhere
         this.gameboyShell.addEventListener("click", ()=> {
-            this.startButton.style.animation = "none"
-            this.startButton.offsetWidth
-            this.startButton.style.animation = "startFlash 0.3s ease 2"
 
-            this.infoButton.style.animation = "none"
-            this.infoButton.offsetWidth
-            this.infoButton.style.animation = "helpFlash 0.3s ease 2"
-        })
+            this.triggerMenuFlashAnimations()
+        }
+            
+        )
 
 
         this.startButton.addEventListener("click", ()=> {
             this.homeScreen.style.display = "none"
-            // remove event listener from gameboy sehll needed to emphasize start button
-            //maybe even add an animation or loading screen while waiting
+            //Add a loading screen while waiting for the start fo the battle to load
             BattleLoader.loadBattle()
         })
     }
 
-    
-    //create a way to emphasize the start button here if the user clicks the gameboy
+    triggerMenuFlashAnimations() {
+        this.startButton.style.animation = "none"
+        this.startButton.offsetWidth
+        this.startButton.style.animation = "startFlash 0.3s ease 2"
+
+        this.infoButton.style.animation = "none"
+        this.infoButton.offsetWidth
+        this.infoButton.style.animation = "helpFlash 0.3s ease 2"
+    }
+
 }
