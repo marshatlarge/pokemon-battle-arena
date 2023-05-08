b62d494f76818a2360f7f4ac01b4fd6defc84f40
manielsen13 <marshall.niels@gmail.com>
2023-05-08 11:56:25 -0400

    Add flash indicators when clicking off screen to menu buttons

--- modules/menu_ui_controller.js
+++ modules/menu_ui_controller.js
@@ -5,17 +5,33 @@ export default class MenuUIController {
 
         this.homeScreen = document.getElementById("homeScreen")
         this.startButton = document.getElementById("startButton")
+        this.infoButton = document.getElementById("infoButton")
+
+        this.gameboyShell = document.getElementById("gbaCasing")
 
     }
 
     configureButtons(){
+
+        this.gameboyShell.addEventListener("click", ()=> {
+            this.startButton.style.animation = "none"
+            this.startButton.offsetWidth
+            this.startButton.style.animation = "startFlash 0.3s ease 2"
+
+            this.infoButton.style.animation = "none"
+            this.infoButton.offsetWidth
+            this.infoButton.style.animation = "helpFlash 0.3s ease 2"
+        })
+
+
         this.startButton.addEventListener("click", ()=> {
             this.homeScreen.style.display = "none"
-            //call function to remove event listener from gameboy needed to emphasize start button
+            // remove event listener from gameboy sehll needed to emphasize start button
             //maybe even add an animation or loading screen while waiting
             BattleLoader.loadBattle()
         })
     }
 
+    
     //create a way to emphasize the start button here if the user clicks the gameboy
 }
